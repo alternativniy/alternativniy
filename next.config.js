507d47c82b73dd9085a8479bd374774a1c4032fb
Роман <rr@ilab.kz>
2021-04-21 19:13:59 +0600

    Добавлен PWA

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/*const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public'
+    dest: 'public',
+	runtimeCaching,
   }
-})*/
+})
 
 const path = require('path')
 
