8acc8778d82feb52017c5de650e7cd5d13b3de3b
Роман <rr@ilab.kz>
2021-04-22 12:45:28 +0600

    Добавлены необходимые пакеты

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
+const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-	runtimeCaching,
-  }
-})
-
 const path = require('path')
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+const nextConfig = {
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	}
 }
+
+module.exports = withPlugins([
+	withPWA({
+		dest: 'public',
+		runtimeCaching,
+	})
+], nextConfig)
